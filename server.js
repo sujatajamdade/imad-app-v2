f78f9c7a8a489a12632f3bb074857eca8168d4fa
sujatajamdade <jamdade21196@gmail.com>
2017-03-19 15:13:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-  res.send('article two is running');
+  //res.send('article two is running');
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
