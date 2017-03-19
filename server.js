924cb390691434d54ded44a089d62f137e122ef1
sujatajamdade <jamdade21196@gmail.com>
2017-03-19 15:11:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
-app.get('/article-two', function (req, res) {
+app.get('/article-one', function (req, res) {
   res.send('article two is running');
 });
 
