008a75f4f121ff44a3447db7394b4e3f2008175c
sujatajamdade <jamdade21196@gmail.com>
2017-03-21 14:15:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML='new value';
+
+var img = document.getElementById('img');
+img.onclick=function(){
+    img=style.marginLeft='100px';
+};
