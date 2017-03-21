21c3ace792039de1cf5e5e5cdab7e8d1740aec44
sujatajamdade <jamdade21196@gmail.com>
2017-03-21 14:29:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML='new value';
 
-var img = document.getElementById('madi');
+var img = document.getElementById('img');
 img.onclick=function(){
     img.style.marginLeft='100px';
 };
