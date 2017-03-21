f730653d6013b8f9bc88b462947c6955dee8ceec
sujatajamdade <jamdade21196@gmail.com>
2017-03-21 14:38:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,12 @@ var element = document.getElementById('main-text');
 element.innerHTML='new value';
 
 var img = document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+     img.style.marginLeft=marginLeft+'px';
+}
 img.onclick=function(){
     img.style.marginLeft='100px';
-    var interval=setInterval(moveLeft,100);
+    var interval=setInterval(moveRight,100);
 };
