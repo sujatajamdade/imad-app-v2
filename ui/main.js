f328e968374ff25e9fde039989fc591cadfc5d4e
sujatajamdade <jamdade21196@gmail.com>
2017-03-22 13:05:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,21 +1,8 @@
-console.log('Loaded!');
-var element = document.getElementById('main-text');
-//element.innerHTML='new value';
-
-var img = document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+1;
-     img.style.marginLeft=marginLeft+'px';
-}
-/*function moveLeft(){
-    marginRight=marginRight+5;
-    img.style.marginRight=marginRight+'px';
-}*/
-img.onclick=function(){
-    //img.style.marginLeft='100px';
-    //var interval=setInterval(moveRight,100);
-   img.style.marginRight='100px';
-    var interval=setInterval(moveLeft,50);
-//};
+var button=document.getElementById('counter');
+var counter=0;
+button.onclick=function()
+{
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
 };
