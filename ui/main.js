524967f3bdaaebb578cdf77d09729f8ed6b10a1c
sujatajamdade <jamdade21196@gmail.com>
2017-03-22 13:23:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,24 @@
 var button=document.getElementById('counter');
-var counter=0;
+//var counter=0;
 button.onclick=function()
 {
-    counter=counter+1;
+   /* counter=counter+1;
     var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
+    span.innerHTML=counter.toString();*/
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function()
+    {
+        if(request.readystate==XMLhttpReqest.DONE)
+        {
+            if(request.status==200)
+            {
+                var counter=request.responseTest;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+                
+            }
+        }
+    };
+    request.open('GET','https://sujatajamdade.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
